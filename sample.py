0a102f78107a47d348281e59558818c76fa7adb5
Kevin Chen <kvchen@berkeley.edu>
2014-02-06 01:30:38 -0800

    update sample.py

--- sample.py
+++ sample.py
@@ -1,3 +1,11 @@
 import pyvse
 
 if __name__ == "__main__":
+    username = "foo@bar.baz"
+    password = "hunter2"
+
+    my_session = pyvse.VSESession(username, password)
+    my_game = Game("mygamename", my_session)
+
+    goog = Stock("STOCK-NSQ-GOOG")
+    my_game.buy(goog, 500)
