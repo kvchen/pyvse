4c5139bdf61ee195e04de196b08e1640aca39a12
Kevin Chen <kvchen@berkeley.edu>
2014-02-06 01:32:07 -0800

    update sample.py

--- sample.py
+++ sample.py
@@ -4,8 +4,10 @@ if __name__ == "__main__":
     username = "foo@bar.baz"
     password = "hunter2"
 
-    my_session = pyvse.VSESession(username, password)
-    my_game = pyvse.Game("mygamename", my_session)
+    my_session = pyvse.VSESession()
+    my_session.login(username, password)
+
+    my_game = my_session.game("mygamename")
 
     goog = pyvse.Stock("STOCK-NSQ-GOOG")
     my_game.buy(goog, 500)
